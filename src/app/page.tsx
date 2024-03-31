961366c9040722087051f6e85c55e06b333c0af6
Subhajit Pramanik <subhajitpramanik1011@gmail.com>
2024-03-31 10:46:01 +0530

    prisma move to src folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,21 @@
-import { PrismaClient } from "@prisma/client";
+"use client";
+
 import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-    const prisma = new PrismaClient();
-
-    async function main() {
-        const users = await prisma.user.findMany();
-        console.log(users);
-    }
-    main()
-        .catch(async (e) => {
-            console.error(e);
-            process.exit(1);
-        })
-        .finally(async () => {
-            await prisma.$disconnect();
+    const onCheckDB = async () => {
+        await fetch("/api/test").then(async (res) => {
+            const data = await res.json();
+            console.log(data.users[0]);
         });
+    };
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <Button variant="ghost">Click me</Button>
+            <Button variant="ghost" onClick={onCheckDB}>
+                Click me
+            </Button>
             <ModeToggle />
         </main>
     );
